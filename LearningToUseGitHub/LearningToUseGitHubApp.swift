c8569602d5dd361620edcfe310c21e6cea387ffa
Edward Critchley <edward.critchley@googlemail.com>
2022-02-24 11:06:44 +0000

    Initial Commit

---
+++ LearningToUseGitHub/LearningToUseGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningToUseGitHubApp.swift
+//  LearningToUseGitHub
+//
+//  Created by Edward Critchley on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningToUseGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
